93ddf1224d4b49d601d25f63c2ceafbf66783db8
woozoobro <woojusm@gmail.com>
2022-10-27 03:46:48 +0900

    Initial Commit

---
+++ MyGumiTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGumiTimer
+//
+//  Created by 우주형 on 2022/10/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
